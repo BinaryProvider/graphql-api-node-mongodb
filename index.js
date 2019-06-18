8a77677a145eab314ed7cd002502e61d6f519ce4
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-17 19:23:44 -0700

    createUser resolver

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const graphql = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,11 +26,22 @@ server.use(
      date: String! 
     }
 
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
+
     input EventInput {
       title: String!
       description: String!
       price: Float!
       date: String!
+    }
+
+    input UserInput {
+      email: String!
+      password: String!
     }
 
     type RootQuery {
@@ -37,6 +50,7 @@ server.use(
 
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
 
     schema {
@@ -73,6 +87,29 @@ server.use(
           })
           .catch(err => {
             console.log(err);
+            throw err;
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error('User already exists.');
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null };
+          })
+          .catch(err => {
             throw err;
           });
       }
