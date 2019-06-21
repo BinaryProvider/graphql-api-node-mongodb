50735e0f7d4d46125cd80895c288d8b891b9c008
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-21 05:49:47 -0700

    refactoring resolver into different files

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,48 @@
+const Event = require('../../models/event');
+const { transformEvent } = require('./merge');
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: dateToString(args.eventInput.date),
+      creator: '5d09415c58bbc362d62e710d'
+    });
+
+    let createdEvent;
+
+    try {
+      const result = await event.save();
+
+      createdEvent = {
+        ...result._doc,
+        date: dateToString(event._doc.date),
+        creator: user.bind(this, result._doc.creator)
+      };
+
+      const creator = await User.findById('5d09415c58bbc362d62e710d');
+
+      if (!creator) {
+        throw new Error('User not found.');
+      }
+
+      creator.createdEvents.push(event);
+      await creator.save();
+      return createdEvent;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
