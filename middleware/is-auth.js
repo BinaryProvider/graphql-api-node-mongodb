6345af43eb6ad31d690545197c3b611ade1b22cf
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-23 21:58:41 -0700

    isauth middleware

---
+++ middleware/is-auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (request, response, next) => {
+  const authHeader = request.get('Authorization');
+  if (!authHeader) {
+    request.isAuth = false;
+    return next();
+  }
+  const token = authHeader.split(' ')[1];
+  if (!token || token === '') {
+    request.isAuth = false;
+    return next();
+  }
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, 'somesupersecretkey');
+  } catch (err) {
+    request.isAuth = false;
+    return next();
+  }
+  if (!decodedToken) {
+    request.isAuth = false;
+    return next();
+  }
+  req.isAuth = true;
+  req.userId = decodedToken.userId;
+  next();
+};
