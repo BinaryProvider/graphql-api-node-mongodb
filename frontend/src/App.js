5895c7b36b7fc97a5e1a0dd238a1252209442eab
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-24 15:43:40 -0700

    switch implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path='/' component={null} />
-      <Route path='/auth' component={null} />
-      <Route path='/events' component={null} />
-      <Route path='/bookings' component={null} />
+      <Switch>
+        <Redirect from='/' to='/auth' exact />
+        <Route path='/' component={null} />
+        <Route path='/auth' component={AuthPage} />
+        <Route path='/events' component={null} />
+        <Route path='/bookings' component={null} />
+      </Switch>
     </BrowserRouter>
   );
 }
