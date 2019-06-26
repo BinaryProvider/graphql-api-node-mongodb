ed3f2cfe866f4ffd88501514e4c40e69dadfd59a
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-26 12:37:22 -0700

    logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,12 +38,12 @@ export default class App extends Component {
             <MainNavigation />
             <main className='main_content'>
               <Switch>
-                {!this.state.token && <Redirect from='/' to='/auth' exact />}
                 {this.state.token && <Redirect from='/' to='/events' exact />}
                 {this.state.token && <Redirect from='/auth' to='/events' exact />}
                 {!this.state.token && <Route path='/auth' component={AuthPage} />}
                 <Route path='/events' component={EventsPage} />
                 {this.state.token && <Route path='/bookings' component={BookingsPage} />}
+                {!this.state.token && <Redirect to='/auth' exact />}
               </Switch>
             </main>
           </AuthContext.Provider>
--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -21,9 +21,14 @@ const MainNavigation = props => (
                 <NavLink to='/events'>Events</NavLink>
               </li>
               {context.token && (
-                <li>
-                  <NavLink to='/bookings'>Bookings</NavLink>
-                </li>
+                <>
+                  <li>
+                    <NavLink to='/bookings'>Bookings</NavLink>
+                  </li>
+                  <li>
+                    <button onClick={context.logout}>Logout</button>
+                  </li>
+                </>
               )}
             </ul>
           </nav>
