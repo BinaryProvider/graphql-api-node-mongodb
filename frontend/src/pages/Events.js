18580ddd839074c037ac573a8bd8342bf8a96387
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-26 22:17:55 -0700

    event refs working

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -8,12 +8,36 @@ export default class Events extends Component {
     creating: false
   };
 
+  constructor(props) {
+    super(props);
+    this.titleElRef = React.createRef();
+    this.priceElRef = React.createRef();
+    this.dateElRef = React.createRef();
+    this.descriptionElRef = React.createRef();
+  }
+
   startCreateEventHandler = () => {
     this.setState({ creating: true });
   };
 
   modalConfirmHandler = () => {
     this.setState({ creating: false });
+    const title = this.titleElRef.current.value;
+    const price = this.priceElRef.current.value;
+    const date = this.dateElRef.current.value;
+    const description = this.descriptionElRef.current.value;
+
+    if (
+      title.trim().length === 0 ||
+      price.trim().length === 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+
+    const event = { title, price, date, description };
+    console.log(event);
   };
 
   modalCancelHandler = () => {
@@ -35,19 +59,19 @@ export default class Events extends Component {
             <form>
               <div>
                 <label htmlFor='title'>Title</label>
-                <input type='text' id='title' />
+                <input type='text' id='title' ref={this.titleElRef} />
               </div>
               <div>
                 <label htmlFor='price'>Price</label>
-                <input type='number' id='price' />
+                <input type='number' id='price' ref={this.priceElRef} />
               </div>
               <div>
                 <label htmlFor='date'>Date</label>
-                <input type='date' id='date' />
+                <input type='date' id='date' ref={this.dateElRef} />
               </div>
               <div>
                 <label htmlFor='description'>Description</label>
-                <textarea id='description' rows='4' />
+                <textarea id='description' rows='4' ref={this.descriptionElRef} />
               </div>
             </form>
           </Modal>
