5afeb5e28745e3e8be127a4f11e4b5211a2c7b27
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-29 01:32:31 -0700

    event data loader

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,11 +1,17 @@
+const DataLoader = require('dataloader');
+
 const Event = require('../../models/event');
 const User = require('../../models/user');
-const { dateToString } = require('../../helpers/date');
+const {dateToString} = require('../../helpers/date');
 
-const events = async eventIds => {
+const eventLoader = new DataLoader((eventIds) => {
+  return events(eventIds);
+});
+
+const events = async (eventIds) => {
   try {
-    const events = await Event.find({ _id: { $in: eventIds } });
-    return events.map(event => {
+    const events = await Event.find({_id: {$in: eventIds}});
+    return events.map((event) => {
       return transformEvent(event);
     });
   } catch (err) {
@@ -13,45 +19,45 @@ const events = async eventIds => {
   }
 };
 
-const singleEvent = async eventId => {
+const singleEvent = async (eventId) => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId);
+    return event;
   } catch (err) {
     throw err;
   }
 };
 
-const user = async userId => {
+const user = async (userId) => {
   try {
     const user = await User.findById(userId);
     return {
       ...user._doc,
       _id: user.id,
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      createdEvents: eventLoader.loadMany.bind(this, user._doc.createdEvents),
     };
   } catch (err) {
     throw err;
   }
 };
 
-const transformEvent = event => {
+const transformEvent = (event) => {
   return {
     ...event._doc,
     _id: event.id,
     date: dateToString(event._doc.date),
-    creator: user.bind(this, event.creator)
+    creator: user.bind(this, event.creator),
   };
 };
 
-const transformBooking = booking => {
+const transformBooking = (booking) => {
   return {
     ...booking._doc,
     _id: booking.id,
     user: user.bind(this, booking._doc.user),
     event: singleEvent.bind(this, booking._doc.event),
     createdAt: dateToString(booking._doc.createdAt),
-    createdAt: dateToString(booking._doc.updatedAt)
+    createdAt: dateToString(booking._doc.updatedAt),
   };
 };
 
