42cfcbd0e6a747a49c755078deee635edb1dc858
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-29 02:25:50 -0700

    auth variables in graphql queries

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import AuthContext from '../context/auth-context';
 
 export default class Auth extends Component {
   state = {
-    isLogin: true
+    isLogin: true,
   };
 
   static contextType = AuthContext;
@@ -15,12 +15,12 @@ export default class Auth extends Component {
   }
 
   switchModeHandler = () => {
-    this.setState(prevState => {
-      return { isLogin: !prevState.isLogin };
+    this.setState((prevState) => {
+      return {isLogin: !prevState.isLogin};
     });
   };
 
-  submitHandler = event => {
+  submitHandler = (event) => {
     event.preventDefault();
     const email = this.emailEl.current.value;
     const password = this.passwordEl.current.value;
@@ -31,26 +31,33 @@ export default class Auth extends Component {
 
     let requestBody = {
       query: `
-        query {
-          login(email: "${email}", password: "${password}") {
+        query Login($email: String!, $password: String!){
+          login(email: $email, password: $password) {
             userId
             token
             tokenExpiration
           }
         }
-      `
+      `,
+      variables: {
+        email,
+        password,
+      },
     };
 
     if (!this.state.isLogin) {
       requestBody = {
         query: `
-          mutation {
-            createUser(userInput: {email: "${email}", password: "${password}"}) {
+          mutation CreateUser($email: String!, $password: String!){
+            createUser(userInput: {email: $email, password: $password}) {
               _id
               email
             }
           }
-        `
+        `,
+        variables: {
+          email,
+        },
       };
     }
 
@@ -58,44 +65,44 @@ export default class Auth extends Component {
       method: 'POST',
       body: JSON.stringify(requestBody),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     })
-      .then(response => {
+      .then((response) => {
         if (response.status !== 200 && response.status !== 201) {
           throw new Error('Failed');
         }
         return response.json();
       })
-      .then(responseData => {
+      .then((responseData) => {
         console.log(responseData);
         if (responseData.data.login.token) {
           this.context.login(
             responseData.data.login.token,
             responseData.data.login.userId,
-            responseData.data.login.tokenExpiration
+            responseData.data.login.tokenExpiration,
           );
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   };
 
   render() {
     return (
-      <form className='auth-form' onSubmit={this.submitHandler}>
-        <div className='form-control'>
-          <label htmlFor='email'>Email</label>
-          <input type='email' id='email' ref={this.emailEl} />
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={this.emailEl} />
         </div>
-        <div className='form-control'>
-          <label htmlFor='password'>Password</label>
-          <input type='password' id='password' ref={this.passwordEl} />
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
         </div>
-        <div className='form-actions'>
-          <button type='submit'>Submit</button>
-          <button type='button' onClick={this.switchModeHandler}>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchModeHandler}>
             Switch to {this.state.isLogin ? 'Signup' : 'Login'}
           </button>
         </div>
